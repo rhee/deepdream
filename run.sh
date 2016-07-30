127b908b7f2d1411beeb64facc275d79890a2e05
sanghoon-rhee <shr386+github@hotmail.com>
2016-07-30 06:56:00 +0000

    .

--- run.sh
+++ run.sh
@@ -4,10 +4,15 @@ MKL_NUM_THREADS=8
 MKL_DOMAIN_NUM_THREADS="MKL_DOMAIN_ALL=1, MKL_DOMAIN_BLAS=8"
 MKL_DYNAMIC=FALSE
 
-guide=guide.jpg
-iter=200
+# lower level than the defualt inception_4c/output,
+# gets more patternish output
+#model=inception_3b/5x5_reduce
+
+# guide image binds the result, not an interesting
+#guide=guide.jpg
+
+iter=2700
 scale=0.05
-model=inception_3b/5x5_reduce
 
 set -e
 set -x
@@ -16,17 +21,14 @@ for input in "$@"; do
 
   b=$(basename $input .jpg)
 
-  sudo -H docker run \
+  sudo --set-home docker run \
     --rm \
     -e MKL_NUM_THREADS=$MKL_NUM_THREADS \
     -e MKL_DOMAIN_NUM_THREADS="$MKL_DOMAIN_NUM_THREADS" \
     -e MKL_DYNAMIC=$MKL_DYNAMIC \
     -v "$PWD":/data \
-    rhee/deepdream python -u /data/deepdream.py --output=$b.output --guide=$guide $input $iter $scale $model
+    rhee/deepdream python -u /data/deepdream.py --output=$b.output --model=$model --guide=$guide $input $iter $scale
 
-  cp $b.output/tmp.prototxt $b.tmp.prototxt
-  ffmpeg -f image2 -r 24 -i $b.output/%04d.jpg -vcodec libx264 $b.output.mp4
-
-  sudo rm -fr $b.output
+  sh run-cleanup.sh "$input"
 
 done
