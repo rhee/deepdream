eb974c1d1420e0fcc4d951546d7efe9132e176e8
sanghoon-rhee <shr386+github@hotmail.com>
2016-07-31 08:17:49 +0000

    .

--- cleanup.sh
+++ cleanup.sh
@@ -5,7 +5,7 @@ for input in "$@"; do
   b=$(basename $input .jpg)
 
   cp $b.output/tmp.prototxt $b.tmp.prototxt
-  ffmpeg -f image2 -r 24 -i $b.output/%04d.jpg -vcodec libx264 $b.output.mp4
+  ffmpeg -f image2 -pattern_type glob -r 24 -i $b.output/'*.jpg' -vcodec libx264 $b.output.mp4
   sudo rm -fr $b.output
 
 done
--- resize-images.sh
+++ resize-images.sh
@@ -1,3 +1,10 @@
 :
-convert input_orig.jpg -colorspace RGB -resize 1024x900\> -colorspace sRGB input.jpg
-convert guide_orig.jpg -colorspace RGB -gravity south -extent 2500x2500 -resize 320x320\> guide.jpg
+if [ -z "$2" ]; then
+  echo "Usage: resize-image.sh input output [geometry]" 1>&2
+  exit 1
+fi
+input="$1"
+output="$2"
+geometry="$3"
+test -z "$geometry" && geometry=1024x576
+convert "$input" -colorspace RGB -resize 2048x1152\> -resize $geometry\> -gravity center -extent $geometry -colorspace sRGB "$output"
