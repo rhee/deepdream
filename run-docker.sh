728434e4ba2e5731a545a63c471f729e0d02263a
sanghoon-rhee <shr386+github@hotmail.com>
2016-08-01 01:41:14 +0000

    .

--- run-docker.sh
+++ run-docker.sh
@@ -1,6 +1,7 @@
 :
 sudo --set-home docker run \
   --rm \
+  --name deepdream-run \
   -e MKL_NUM_THREADS=$MKL_NUM_THREADS \
   -e MKL_DOMAIN_NUM_THREADS="$MKL_DOMAIN_NUM_THREADS" \
   -e MKL_DYNAMIC=$MKL_DYNAMIC \
--- run-notebook.sh
+++ run-notebook.sh
@@ -1,3 +1,2 @@
 :
-#sudo --set-home docker run --name=dream-notebook --rm -t -i -p 8888:8888 -v "$PWD":/data rhee/deepdream jupyter notebook --ip=0.0.0.0
-sudo --set-home docker run --name=dream-notebook --rm -t -i -p 8888:8888 -v "$PWD":/data rhee/deepdream bash
+sudo --set-home docker run --name=dream-notebook --rm -t -i -p 8888:8888 -v "$PWD":/data rhee/deepdream ${1-jupyter notebook --ip=0.0.0.0}
