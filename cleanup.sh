e9877d4b901734adcfbfb80ee4af511029d96b5f
Sang-Hoon RHEE <shr386+github@hotmail.com>
2016-08-06 09:09:10 +0900

    .

--- cleanup.sh
+++ cleanup.sh
@@ -1,15 +1,11 @@
 #!/bin/sh
 
-_ffmpeg=ffmpeg
-avconv --help >/dev/null 2>&1 && _ffmpeg=avconv
-
 for input in "$@"; do
 
   b=$(basename $input .jpg)
 
   cp $b.output/prototxt $b.prototxt
-  $_ffmpeg -f image2 -r 24 -i $b.output/'%04d.jpg' -vcodec libx264 -pix_fmt yuv420p -profile:v baseline $b.output.mp4 </dev/null &&
-    sudo rm -fr $b.output
+  ./make-movie.sh "$b.output" "$b.output.mp4" &&
+    rm -fr $b.output
 
 done
-
--- make-movie.sh
+++ make-movie.sh
@@ -12,10 +12,17 @@ output_mp4="$2"
 setopt shwordsplit 2>/dev/null
 shopt -s nullglob 2>/dev/null
 
-ffmpeg=ffmpeg
+_ffmpeg=ffmpeg
 avconv --help >/dev/null 2>&1 && ffmpeg=avconv
 
 ###
 
-#exec $ffmpeg -f image2 -pattern_type glob -r 24 -i "$input_dir"'/*.jpg' -vcodec libx264 "$output_mp4"
-exec $ffmpeg -f image2 -r 24 -i "$input_dir"'/%04d.jpg' -vcodec libx264 -pix_fmt yuv420p -profile:v baseline "$output_mp4" </dev/null
+_ffmpeg_threads="-threads 8"
+_ffmpeg_input="-f image2 -r 24 -i '$input_dir/%04d.jpg'"
+_ffmpeg_quality="-i_qfactor 0.71 -qcomp 0.6 -qmin 10 -qmax 63 -qdiff 4 -trellis 0"
+_ffmpeg_output="-vcodec libx264 -pix_fmt yuv420p -profile:v baseline -b:v 1000k"
+_ffmpeg_twopass="-movflags faststart"
+
+#ffmpeg -threads 4 -i input_video.mts -i_qfactor 0.71 -qcomp 0.6 -qmin 10 -qmax 63 -qdiff 4 -trellis 0 -vcodec libx264 -s 640x360 -b:v 1000k -b:a 56k -ar 22050 output_video.mp4
+
+exec $_ffmpeg $_ffmpeg_threads $_ffmpeg_input $_ffmpeg_quality $_ffmpeg_output "$output_mp4" $ffmpeg_twopass </dev/null
